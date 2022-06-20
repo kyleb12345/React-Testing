31db9017dc63f467a90097dfd6fc127bdc0fd51f
kyle <bartscholar@gmail.com>
2022-06-20 13:21:09 -0500

    first test

---
+++ src/components/Greeting.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Greeting() {
+  return (
+    <div>
+        <h2>Hello world!</h2>
+        <p>I'm learning about testing</p>
+    </div>
+  )
+}
---
+++ src/components/Greeting.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import Greeting from './Greeting';
+
+test('renders Hello World! as a text', () => {
+    //Arrange - set up test data, condition, and environment
+    render(<Greeting />);
+
+    //Act- run logic that should be tested, ex. execute function
+    //nothing yet
+
+    // Assert- compare execution results with expected results
+    screen.getByText('Hello World!')
+});
