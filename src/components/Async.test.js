4563ed3afd99672539675ed37126d39029ec06c3
kyle <bartscholar@gmail.com>
2022-06-20 14:46:57 -0500

    using a mock for apis

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -4,6 +4,10 @@ import Async from './Async';
 describe('Async component', () => {
     test('renders posts if request succeeds', async () => {
         //Arrange
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{ id: 'p1', title: 'First post'}],
+        });
         render(<Async />)
 
         //Act
@@ -15,6 +19,10 @@ describe('Async component', () => {
         //findAllByRole can have 3 arguements, including a time out
         //now at the top, need to make testing function async
         //thus can add await before screen.findAllByRole
+        //however, this is not ideal because we are sending an http request here, may cause too many API requests to server
+        // posts requests could also send data we do not want since we are testing
+        // window.fetch = jest.fn(); helps create a mock dummy and then: window.fetch.mockResolvedValueOnce();
+        // fill in the mock with a function based on json data function and array of fake data
         const listItemElements = await screen.findAllByRole('listitem');
         expect(listItemElements).not.toHaveLength(0);
     });
